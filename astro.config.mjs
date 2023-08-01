d5577f65628ddc949599dc8d5bc28adc4d795dd0
Nuno Pereira <nunoafonso2002@gmail.com>
2023-08-01 12:24:54 +0100

    Removed custom styles, moving to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://naapperas.github.io',
+  site: 'https://naapperas.github.io',
+  integrations: [tailwind()]
 });
