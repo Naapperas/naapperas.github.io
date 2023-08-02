e485c68a600c67b1a569253df1e5d2b5e6bfb55e
Nuno Pereira <nunoafonso2002@gmail.com>
2023-08-02 02:18:21 +0100

    Added tailwind, added new page for CV; styled AppBar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"color-1": '#2f2f2f',
+				"color-2": '#e1b07e',
+				"color-3": '#e5be9e',
+				"color-4": '#cbc0ad',
+				"color-5": '#86a397',
+			}
+		},
 	},
 	plugins: [],
 }
