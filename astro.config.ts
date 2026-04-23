ac9d3559e90071c8f9e7e47ca9116783de569043
Nuno Pereira <nunopereira@cloudflare.com>
2026-04-23 19:08:55 +0100

    Fix astro-icons breaking builds here lmao

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://naapperas.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
